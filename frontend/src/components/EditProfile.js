2ce049a7d9f1653a4596ba96aacab1bf94b38839
NirmalSeneca <nmpanchal2@myseneca.ca>
2022-08-13 11:19:07 -0400

    Resolving the issues due to merge

--- frontend/src/components/EditProfile.js
+++ frontend/src/components/EditProfile.js
@@ -1,54 +1,56 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect} from "react";
 import Button from "react-bootstrap/Button";
 import { UserContext } from "../contexts/UserContext";
 
 const EditProfile = () => {
   const { user } = useContext(UserContext);
-  const [updatedUser, setUpdatedUsers] = useState({});
   const [loggedInUser, setLoggedInUser] = useState({});
+  const [updatedUser, setUpdatedUsers] = useState({fname:'',lname:'',email:''});
+  const [msg,setMsg] = useState('');
   const handleChange = (event) => {
-    const name = event.target.name;
-
+    const name = event.target.name
     const value = event.target.value;
     setUpdatedUsers((users) => ({ ...users, [name]: value }));
   };
-  const submitHandler = (evt) => {
-    evt.preventDefault();
+
+  useEffect(() => {
     fetch(`/api/user/getUserByEmail/${user.email}`)
       .then((res) => {
         return res.json();
       })
-      .then((json) => {
-        console.log(json);
-        setLoggedInUser(json.users[0]);
-        console.log(json.users[0]);
-        fetch(
-          `/api/user/updateUser/${json.users[0]._id}`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+      .then((json)=>{
+        setUpdatedUsers(json.users[0]);
+      })
+      .catch(err=>{
+        console.log(`Error ${err}`);
+      })
+  },[])
 
-            body: JSON.stringify(user),
-          }
-        )
+  //#region UpdateUser
+  const updateUser = (evt) => {
+    evt.preventDefault();
+    fetch(`/api/user/updateUser/${updatedUser._id}`,
+          { method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updatedUser),
+          })
           .then((res) => {
             if (res.ok) {
-              console.log("User updated");
-            } else {
-              console.log("user not updated");
+              return res.json();
             }
+          })
+          .then((json)=>{
+            setMsg(json.message);
           })
           .catch((err) => {
             console.log(`Error ${err}`);
           });
-      })
-      .catch((err) => {
-        console.log(`Error ${err}`);
-      });
-  };
+  }
+  //#endregion
+
   return (
     <div>
-      <form noValidate onSubmit={submitHandler}>
+      <form noValidate onSubmit={updateUser}>
         <div className="form">
           <div className="form-body">
             <div className="username">
@@ -79,6 +81,18 @@ const EditProfile = () => {
                 placeholder="LastName"
               />
             </div>
+            {/* <div className="phone#">
+              <label className="form__label" for="lastName">
+                Phone number:
+              </label>
+              <input
+                type="text"
+                name=""
+                id="phone#"
+                className="form__input"
+                placeholder="Phone #"
+              />
+            </div> */}
             <div className="email">
               <label className="form__label" for="email">
                 Email:
@@ -93,11 +107,36 @@ const EditProfile = () => {
                 placeholder="Email"
               />
             </div>
+            {/* <div className="password">
+              <label className="form__label" for="password">
+                Password:
+              </label>
+              <input
+                className="form__input"
+                type="password"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="confirm-password">
+              <label className="form__label" for="confirmPassword">
+                Confirm Password:
+              </label>
+              <input
+                className="form__input"
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+              />
+            </div> */}
           </div>
           <div class="footer">
             <Button type="submit" class="btn" variant="outline-primary">
               Update Details
             </Button>
+          </div>
+          <div>
+            {msg}
           </div>
         </div>
       </form>
--- frontend/src/components/ViewCancelAppointment.js
+++ frontend/src/components/ViewCancelAppointment.js
@@ -1,44 +1,45 @@
-import { Button, MenuItem, TextField } from "@mui/material";
-import React, { useEffect, useState, useContext } from "react";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import CancelIcon from "@mui/icons-material/Cancel";
-import { UserContext } from "../contexts/UserContext";
-
+import { Button, MenuItem, TextField } from '@mui/material'
+import React, { useEffect, useState, useContext } from 'react'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { UserContext } from '../contexts/UserContext';
 const ViewCancelAppointment = () => {
+
   const [appointmentDate, setAppointmentDate] = useState(null);
   const [barberNames, setBarberNames] = React.useState([]);
-  const [barberName, setBarberName] = useState("");
+  const [barberName, setBarberName] = useState('');
   const [showTable, setShowTable] = useState(false);
   const [appointment, setAppointment] = useState([]);
-  const [msg, setMsg] = useState("");
+  const [msg, setMsg] = useState('');
 
   const { user } = useContext(UserContext);
 
+  //#region BarberNamesPopulate
   useEffect(() => {
     fetch("/api/user/getUserByType/bb")
       .then((res) => {
         return res.json();
       })
       .then((json) => {
-        setBarberNames(json.user);
+        setBarberNames(json.user)
       })
       .catch((err) => {
         console.log(`Error ${err}`);
       });
-  }, []);
+  }, [])
+  //#endregion
 
+  //#region CancelAllAppointment
+  //Cancel all the Appointment for a particular day based on Date and Barber
   const onCancelAllAppointment = () => {
-    fetch(
-      `/api/appointment/deleteAppointmentByDateAndBarber?date=${appointmentDate}&barber=${barberName}`,
-      { method: "PUT" }
-    )
+    fetch(`/api/appointment/deleteAppointmentByDateAndBarber?date=${appointmentDate}&barber=${barberName}`, { method: "PUT" })
       .then((res) => {
         if (res.status === 200) {
           if (res.json().updateresult.modifiedCount === appointment.length) {
@@ -49,23 +50,24 @@ const ViewCancelAppointment = () => {
       .catch((err) => {
         console.log(err.message);
       });
-    setShowTable(true);
-  };
+    setShowTable(true)
+  }
+  //#endregion
 
+  //#region FetchAllAppointments
+  //All Appointments for particular date and Barber Name
   const getAppointments = (evt) => {
     evt.preventDefault();
-    if (appointmentDate != null && barberName != "") {
-      setMsg("");
-      fetch(
-        `/api/appointment/getAppointmentsByDateAndBarber?date=${appointmentDate}&barber=${barberName}`
-      )
+    if (appointmentDate != null && barberName != '') {
+      setMsg('');
+      fetch(`/api/appointment/getAppointmentsByDateAndBarber?date=${appointmentDate}&barber=${barberName}`)
         .then((res) => {
           return res.json();
         })
         .then((json) => {
           if (json.message) {
             setShowTable(false);
-            setMsg(json.message);
+            setMsg(json.message)
           } else {
             setShowTable(true);
             setAppointment(json.appointments);
@@ -76,29 +78,31 @@ const ViewCancelAppointment = () => {
         });
     } else {
       if (appointmentDate == null) {
-        setMsg("Appointment Date is missing");
+        setMsg('Appointment Date is missing');
       } else {
-        setMsg("Barbers Name is not Selected");
+        setMsg('Barbers Name is not Selected')
       }
     }
-  };
 
+  }
+  //#endregion
+
+  //#region CancelSingleAppointment
+  //Cancel Single Appointment and Update all appointment array
   const onCancelSingleAppointment = (event, bookingId) => {
     fetch(`api/appointment/deleteAppointment?id=${bookingId}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: null,
-    });
-    fetch(
-      `/api/appointment/getAppointmentsByDateAndBarber?date=${appointmentDate}&barber=${barberName}`
-    )
+      body: null
+    })
+    fetch(`/api/appointment/getAppointmentsByDateAndBarber?date=${appointmentDate}&barber=${barberName}`)
       .then((res) => {
         return res.json();
       })
       .then((json) => {
         if (json.message) {
-          setShowTable(false);
-          setMsg(json.message);
+          setShowTable(false)
+          setMsg(json.message)
         } else {
           setAppointment(json.appointments);
         }
@@ -106,46 +110,38 @@ const ViewCancelAppointment = () => {
       .catch((err) => {
         console.log(err.message);
       });
-  };
+  }
+  //#endregion
 
   return (
     <div>
-      <div className="viewcancelapp">
+      <div className='viewcancelapp'>
         <form noValidate onSubmit={getAppointments}>
           <div>
-            <div className="viewfield">
+            <div className='viewfield'>
               <DatePicker
                 label="Appointment Date"
                 value={appointmentDate}
                 onChange={(newValue) => {
-                  setAppointmentDate(newValue);
                   var date = new Date();
-                  var d = new Date(
-                    newValue.getFullYear() +
-                      "-" +
-                      (newValue.getMonth() + 1) +
-                      "-" +
-                      (newValue.getDate() +
-                        " " +
-                        new Date().getHours() +
-                        ":" +
-                        new Date().getMinutes())
-                  );
+                  var d = new Date(newValue.getFullYear() +
+                    "-" + (newValue.getMonth() + 1) +
+                    "-" + (newValue.getDate() +
+                      " " + new Date().getHours() +
+                      ":" + new Date().getMinutes()));
                   d.setHours(d.getHours() + 4);
-                  console.log(d);
+                  console.log(d)
                   d.setHours(0, 0, 0, 0);
                   setAppointmentDate(d);
                 }}
                 renderInput={(params) => <TextField {...params} />}
               />
             </div>
-            <div className="viewfield">
+            <div className='viewfield'>
               <TextField
                 select
                 label="Barber's Name"
-                onChange={(event) => {
-                  setBarberName(event.target.value);
-                }}
+                onChange={(event) => { setBarberName(event.target.value) }}
                 helperText="Please select Barber's Name"
               >
                 {barberNames.map((option) => (
@@ -156,72 +152,58 @@ const ViewCancelAppointment = () => {
               </TextField>
             </div>
           </div>
-          <div className="book">
-            <Button variant="contained" type="submit">
-              Get Appointments
-            </Button>
+          <div className='book'>
+            <Button
+              variant="contained"
+              type='submit'
+            >Get Appointments</Button>
           </div>
         </form>
       </div>
-      <div className="tablecontent">
-        <div className="msg">{msg}</div>
-        {showTable && (
-          <div>
-            <TableContainer component={Paper}>
-              <Table
-                sx={{ minWidth: 600, color: "#f5deb3" }}
-                aria-label="simple table"
-              >
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Sr No</TableCell>
-                    <TableCell align="center">Booking Id</TableCell>
-                    <TableCell align="center">Customer Name</TableCell>
-                    <TableCell align="center">Date</TableCell>
-                    <TableCell align="center">Time</TableCell>
-                    <TableCell align="center">
-                      <Button
-                        variant="contained"
-                        type="button"
-                        color="secondary"
-                        onClick={onCancelAllAppointment}
-                      >
-                        Cancel All
-                      </Button>
+      <div className='tablecontent'>
+        <div className='msg'>{msg}</div>
+        {showTable && <div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 600, color: '#f5deb3' }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Sr No</TableCell>
+                  <TableCell align="center">Booking Id</TableCell>
+                  <TableCell align="center">Customer Name</TableCell>
+                  <TableCell align="center">Date</TableCell>
+                  <TableCell align="center">Time</TableCell>
+                  <TableCell align="center">
+                    <Button variant="contained"
+                      type='button' color='secondary' onClick={onCancelAllAppointment}>Cancel All</Button>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {appointment.map((row, index) => (
+                  <TableRow
+                    key={row.bookingId}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {index + 1}
                     </TableCell>
+                    <TableCell align="center">{row._id}</TableCell>
+                    <TableCell align="center">{row.customer.fname}</TableCell>
+                    <TableCell align="center">{row.date}</TableCell>
+                    <TableCell align="center">{row.time}</TableCell>
+                    <TableCell align="center">
+                      <Button onClick={(event) => onCancelSingleAppointment(event, row._id)} startIcon={<CancelIcon />}></Button>
+                    </TableCell>
+
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {appointment.map((row, index) => (
-                    <TableRow
-                      key={row.bookingId}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {index + 1}
-                      </TableCell>
-                      <TableCell align="center">{row._id}</TableCell>
-                      <TableCell align="center">{row.customer.fname}</TableCell>
-                      <TableCell align="center">{row.date}</TableCell>
-                      <TableCell align="center">{row.time}</TableCell>
-                      <TableCell align="center">
-                        <Button
-                          onClick={(event) =>
-                            onCancelSingleAppointment(event, row._id)
-                          }
-                          startIcon={<CancelIcon />}
-                        ></Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
-        )}
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ViewCancelAppointment;
+export default ViewCancelAppointment
